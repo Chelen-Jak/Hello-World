829eba3950df37db2e512d48b2e608d7439cf6e5
Chelen-Jak <105348628@qq.com>
2016-04-01 09:27:03 +0800

    20160401更改

---
+++ hello_world.js
@@ -0,0 +1,17 @@
+changeRed = function() {
+	var arrTags = document.getElementsByTagName("h1"); 
+	var l = arrTags.length;
+	
+	for (var i = 0; i < l; i++) {
+		arrTags[i].className = "color-red";
+	}
+}
+
+changeBlue = function() {		
+	var arrTags = document.getElementsByTagName("h1"); 
+	var l = arrTags.length;
+	
+	for (var i = 0; i < l; i++) {
+		document.getElementsByTagName("h1")[i].className = "color-blue";
+	}
+}
