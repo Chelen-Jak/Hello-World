baf38c324f505fa3592ea11bf615b112bb3e5856
Chelen-Jak <105348628@qq.com>
2016-04-02 11:33:24 +0800

    Revert "20160402测试"
    This reverts commit e8125065b5442939845af2c54b51632a0eb3a9ce.

--- hello_world.js
+++ hello_world.js
@@ -15,13 +15,3 @@ changeBlue = function() {
 		document.getElementsByTagName("h1")[i].className = "color-blue";
 	}
 }
-
-changeGreen = function() {
-	var arrTags = document.getElementsByTagName("h1");
-	var l = arrTags.length;
-	
-	for (var i = 0; i < l; i++) {
-		document.getElementsByTagName("h1")[i].className = "color-green";
-	}	
-}
-
